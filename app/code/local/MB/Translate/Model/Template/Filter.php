8676a0f533cde48ea331f1aa9e3ccb3f6c742acb
Marius Strajeru <marius.strajeru@gmail.com>
2014-02-10 11:06:38 +0200

    #2

--- app/code/local/MB/Translate/Model/Template/Filter.php
+++ app/code/local/MB/Translate/Model/Template/Filter.php
@@ -2,10 +2,50 @@
 
 class MB_Translate_Model_Template_Filter extends Mage_Widget_Model_Template_Filter
 {
+    const XML_DEFAULT_MODULE_PATH = 'cms/translate/default_module';
+    const DEFAULT_MODULE          = 'page';
+    protected $_defaultModule     = null;
     public function translateDirective($construction)
     {
         $params = $this->_getIncludeParameters($construction[2]);
         $text = $params['text'];
-        return Mage::helper('page')->__($text);
+        $module = (!empty($params['module'])) ? $this->_getTranslateModuleName($params['module']) : $this->_getDefaultModule();
+        return Mage::helper($module)->__($module.'--'.$text);
+    }
+    protected function _getTranslateModuleName($module)
+    {
+        try
+        {
+            $helper = Mage::helper($module);
+            return $module;
+        }
+        catch (Exception $e)
+        {
+            return $this->_getDefaultModule();
+        }
+    }
+    protected function _getDefaultModule()
+    {
+        if (is_null($this->_defaultModule))
+        {
+            $moduleName = Mage::getStoreConfig(self::XML_DEFAULT_MODULE_PATH);
+            if (empty($moduleName))
+            {
+                $this->_defaultModule = self::DEFAULT_MODULE;
+            }
+            else
+            {
+                try
+                {
+                    $helper = Mage::helper($moduleName);
+                    $this->_defaultModule = $moduleName;
+                }
+                catch (Exception $e)
+                {
+                    $this->_defaultModule = self::DEFAULT_MODULE;
+                }
+            }
+        }
+        return $this->_defaultModule;
     }
 }
